09330c1b6b88e7b5d2440ecf8b9a7ac2d5778f0d
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-08-08 08:35:51 -0700

    WIP, tinkering with NF shenanigans

--- neoforge/src/main/kotlin/us/timinc/mc/cobblemon/timcore/neoforge/AbstractNeoForgeMod.kt
+++ neoforge/src/main/kotlin/us/timinc/mc/cobblemon/timcore/neoforge/AbstractNeoForgeMod.kt
@@ -1,19 +1,26 @@
-@file:Suppress("unused")
-
 package us.timinc.mc.cobblemon.timcore.neoforge
 
 import net.neoforged.neoforge.common.NeoForge
+import net.neoforged.neoforge.event.AddReloadListenerEvent
 import net.neoforged.neoforge.event.RegisterCommandsEvent
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 import us.timinc.mc.cobblemon.timcore.AbstractMod
 
 abstract class AbstractNeoForgeMod(@Suppress("MemberVisibilityCanBePrivate") val mod: AbstractMod<*>) {
     init {
         NeoForge.EVENT_BUS.addListener(::registerCommands)
+        MOD_BUS.addListener(::registerReloadListeners)
         mod.wrapUp()
     }
 
     private fun registerCommands(e: RegisterCommandsEvent) {
         val dispatcher = e.dispatcher
         mod.commands.forEach(dispatcher::register)
+    }
+
+    private fun registerReloadListeners(e: AddReloadListenerEvent) {
+        mod.reloadListeners.forEach { listener ->
+            e.addListener(NeoForgeReloadListener(listener))
+        }
     }
 }
