9b3cef6bb16332283d2aefad9610573e93fb04b6
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-08-07 18:13:24 -0700

    clean up

--- common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/handler/AttachBucket.kt
+++ common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/handler/AttachBucket.kt
@@ -6,7 +6,7 @@ import us.timinc.mc.cobblemon.timcore.AbstractHandler
 import us.timinc.mc.cobblemon.timcore.TimCore.DataKeys.SPAWNED_IN_BUCKET
 import us.timinc.mc.cobblemon.timcore.TimCore.config
 
-object AttachBucket: AbstractHandler<SpawnEvent<PokemonEntity>>() {
+object AttachBucket : AbstractHandler<SpawnEvent<PokemonEntity>>() {
     override fun handle(evt: SpawnEvent<PokemonEntity>) {
         if (!config.addBucketToData) return
         evt.entity.pokemon.persistentData.putString(SPAWNED_IN_BUCKET, evt.ctx.cause.bucket.name)
