cce167697e56e05f90b556042631f6530dc64f1d
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-10-04 23:27:25 -0700

    more cleanup

--- common/src/main/java/us/timinc/mc/cobblemon/timcore/mixins/BerryBlockEntityFix.java
+++ common/src/main/java/us/timinc/mc/cobblemon/timcore/mixins/BerryBlockEntityFix.java
@@ -1,25 +1,22 @@
 package us.timinc.mc.cobblemon.timcore.mixins;
 
-import com.cobblemon.mod.common.api.berry.Berry;
-import com.cobblemon.mod.common.api.berry.GrowthPoint;
-import com.cobblemon.mod.common.block.entity.BerryBlockEntity;
-import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import us.timinc.mc.cobblemon.timcore.mixin.helper.BerryBlockEntityFixHelper;
-import kotlin.Pair;
-import java.util.ArrayList;
-import java.util.List;
+import com.cobblemon.mod.common.api.berry.*;
+import com.cobblemon.mod.common.block.entity.*;
+import kotlin.*;
+import net.minecraft.resources.*;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.*;
+import us.timinc.mc.cobblemon.timcore.mixin.helper.*;
+
+import java.util.*;
 
 @Mixin(BerryBlockEntity.class)
 public abstract class BerryBlockEntityFix {
     @Shadow
     private String growthPointSequence;
 
-    @org.spongepowered.asm.mixin.Final
+    @Final
     @Shadow
     private ArrayList<ResourceLocation> growthPoints;
 
