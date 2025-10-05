6b6d77cff38959e7a239b2892cb0e8d0e418f69a
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-10-04 23:26:45 -0700

    clean up

--- common/src/main/java/us/timinc/mc/cobblemon/timcore/mixins/BerryBlockEntityFix.java
+++ common/src/main/java/us/timinc/mc/cobblemon/timcore/mixins/BerryBlockEntityFix.java
@@ -1,31 +1,33 @@
 package us.timinc.mc.cobblemon.timcore.mixins;
 
-import com.cobblemon.mod.common.entity.pokeball.*;
-import com.cobblemon.mod.common.entity.pokemon.*;
-import net.minecraft.network.syncher.*;
-import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.projectile.*;
-import net.minecraft.world.level.*;
-import net.minecraft.world.phys.*;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.*;
-import us.timinc.mc.cobblemon.timcore.mixin.helper.*;
+import com.cobblemon.mod.common.api.berry.Berry;
+import com.cobblemon.mod.common.api.berry.GrowthPoint;
 import com.cobblemon.mod.common.block.entity.BerryBlockEntity;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import us.timinc.mc.cobblemon.timcore.mixin.helper.BerryBlockEntityFixHelper;
+import kotlin.Pair;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(BerryBlockEntity.class)
 public abstract class BerryBlockEntityFix {
     @Shadow
     private String growthPointSequence;
 
+    @org.spongepowered.asm.mixin.Final
     @Shadow
-    private java.util.ArrayList<net.minecraft.resources.ResourceLocation> growthPoints;
+    private ArrayList<ResourceLocation> growthPoints;
 
     @Shadow
-    protected abstract com.cobblemon.mod.common.api.berry.Berry berry();
+    public abstract Berry berry();
 
     @Inject(method = "berryAndGrowthPoint$common", at = @At(value = "HEAD"), remap = false, cancellable = true)
-    void fixBerryAndGrowthPoint(CallbackInfoReturnable<java.util.List<kotlin.Pair<com.cobblemon.mod.common.api.berry.Berry, com.cobblemon.mod.common.api.berry.GrowthPoint>>> cir) {
+    void fixBerryAndGrowthPoint(CallbackInfoReturnable<List<Pair<Berry, GrowthPoint>>> cir) {
         cir.setReturnValue(BerryBlockEntityFixHelper.process(this.berry(), this.growthPointSequence, this.growthPoints));
         cir.cancel();
     }
