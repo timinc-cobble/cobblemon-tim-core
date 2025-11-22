b8993fe3e33edb37e09b0c0b7bf777fc1fb3ccdb
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-11-21 18:47:30 -0800

    rushing out a 1.7.0 version by removing the network functionality for later implementation

--- common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/OwoClientNetworkHandler.kt
+++
@@ -1,6 +0,0 @@
-package us.timinc.mc.cobblemon.timcore
-
-interface OwoClientNetworkHandler<Packet : Record> {
-    val clientPacketClass: Class<Packet>
-    fun handleClient(data: Packet, clientAccess: Any)
-}
--- common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/OwoServerNetworkHandler.kt
+++
@@ -1,6 +0,0 @@
-package us.timinc.mc.cobblemon.timcore
-
-interface OwoServerNetworkHandler<Packet : Record> {
-    val serverPacketClass: Class<Packet>
-    fun handleServer(data: Packet, serverAccess: Any)
-}
