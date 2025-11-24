180ff3d0dfbd159cd8627fb79a7e1b69ed809b44
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-11-23 17:56:17 -0800

    clean up

--- common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/event/EntityLoadEvent.kt
+++ common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/event/EntityLoadEvent.kt
@@ -3,7 +3,7 @@ package us.timinc.mc.cobblemon.timcore.event
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.Entity
 
-data class EntityLoadEvent<T : Entity> (
+data class EntityLoadEvent<T : Entity>(
     val entity: T,
     val world: ServerLevel,
 )
