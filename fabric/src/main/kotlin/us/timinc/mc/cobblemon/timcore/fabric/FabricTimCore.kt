eef3ca3a24a2f877a5d2dcb5080ac7f483479c0b
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-11-24 07:18:07 -0800

    added entity unload event and filtered Pokemon version

--- fabric/src/main/kotlin/us/timinc/mc/cobblemon/timcore/fabric/FabricTimCore.kt
+++ fabric/src/main/kotlin/us/timinc/mc/cobblemon/timcore/fabric/FabricTimCore.kt
@@ -4,12 +4,21 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents
 import us.timinc.mc.cobblemon.timcore.TimCore
 import us.timinc.mc.cobblemon.timcore.TimCoreEvents
 import us.timinc.mc.cobblemon.timcore.event.EntityLoadEvent
+import us.timinc.mc.cobblemon.timcore.event.EntityUnloadEvent
 
 object FabricTimCore : AbstractFabricMod(TimCore) {
     override fun onInitialize() {
         ServerEntityEvents.ENTITY_LOAD.register { entity, world ->
             TimCoreEvents.ENTITY_LOAD.post(
                 EntityLoadEvent(
+                    entity,
+                    world
+                )
+            )
+        }
+        ServerEntityEvents.ENTITY_UNLOAD.register { entity, world ->
+            TimCoreEvents.ENTITY_UNLOAD.post(
+                EntityUnloadEvent(
                     entity,
                     world
                 )
--- neoforge/src/main/kotlin/us/timinc/mc/cobblemon/timcore/neoforge/NeoForgeTimCore.kt
+++ neoforge/src/main/kotlin/us/timinc/mc/cobblemon/timcore/neoforge/NeoForgeTimCore.kt
@@ -4,22 +4,34 @@ import net.minecraft.server.level.ServerLevel
 import net.neoforged.fml.common.Mod
 import net.neoforged.neoforge.common.NeoForge
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent
+import net.neoforged.neoforge.event.entity.EntityLeaveLevelEvent
 import us.timinc.mc.cobblemon.timcore.MOD_ID
 import us.timinc.mc.cobblemon.timcore.TimCore
 import us.timinc.mc.cobblemon.timcore.TimCoreEvents
 import us.timinc.mc.cobblemon.timcore.event.EntityLoadEvent
+import us.timinc.mc.cobblemon.timcore.event.EntityUnloadEvent
 
 @Mod(MOD_ID)
 object NeoForgeTimCore : AbstractNeoForgeMod(TimCore) {
     init {
         with(NeoForge.EVENT_BUS) {
             addListener(::onEntityLoad)
+            addListener(::onEntityUnload)
         }
     }
 
     fun onEntityLoad(e: EntityJoinLevelEvent) {
         TimCoreEvents.ENTITY_LOAD.post(
             EntityLoadEvent(
+                e.entity,
+                e.level as? ServerLevel ?: return
+            )
+        )
+    }
+
+    fun onEntityUnload(e: EntityLeaveLevelEvent) {
+        TimCoreEvents.ENTITY_UNLOAD.post(
+            EntityUnloadEvent(
                 e.entity,
                 e.level as? ServerLevel ?: return
             )
