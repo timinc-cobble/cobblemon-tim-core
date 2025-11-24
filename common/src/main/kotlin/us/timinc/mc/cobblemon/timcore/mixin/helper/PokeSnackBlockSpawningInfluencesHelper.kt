b4fb968767c905a88b42b7fb71fe0e1062cd662d
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-11-23 17:55:07 -0800

    moved the snack did spawn logic to use a list of already injected because I wasn't confident in the boolean on the mixin. moved mixing to head so it didn't miss the first one (d'oh).

--- common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/mixin/helper/PokeSnackBlockSpawningInfluencesHelper.kt
+++ common/src/main/kotlin/us/timinc/mc/cobblemon/timcore/mixin/helper/PokeSnackBlockSpawningInfluencesHelper.kt
@@ -4,7 +4,16 @@ import com.cobblemon.mod.common.block.entity.PokeSnackBlockEntity
 import us.timinc.mc.cobblemon.timcore.TimCore
 
 object PokeSnackBlockSpawningInfluencesHelper {
+    val alreadyInjected: MutableSet<PokeSnackBlockEntity> = mutableSetOf()
+
     fun handle(entity: PokeSnackBlockEntity) {
+        cleanUp()
+        if (alreadyInjected.contains(entity)) return
         entity.spawner.influences.addAll(TimCore.snackSpawnerInfluences)
+        alreadyInjected.add(entity)
+    }
+
+    fun cleanUp() {
+        alreadyInjected.removeIf { it.isRemoved }
     }
 }
